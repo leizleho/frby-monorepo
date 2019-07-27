3d4791c612acb9131bfe5d0604f88f09a8f7d877
leizleho <ho.leizle@gmail.com>
2019-07-27 10:39:58 -0700

    login controller

--- packages/controller/src/modules/schemaTypes.ts
+++ packages/controller/src/modules/schemaTypes.ts
@@ -3,6 +3,35 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL mutation operation: LoginMutation
+// ====================================================
+
+export interface LoginMutation_login_errors {
+  __typename: "Error";
+  path: string;
+  message: string;
+}
+
+export interface LoginMutation_login {
+  __typename: "LoginResponse";
+  errors: LoginMutation_login_errors[] | null;
+  sessionId: string | null;
+}
+
+export interface LoginMutation {
+  login: LoginMutation_login;
+}
+
+export interface LoginMutationVariables {
+  email: string;
+  password: string;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL mutation operation: RegisterMutation
 // ====================================================
 
