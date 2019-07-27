7706f05cf048be05bdf86e9772c9abfed6ca371a
leizleho <ho.leizle@gmail.com>
2019-07-27 12:23:35 -0700

    ChangePassword controller

--- packages/controller/src/modules/schemaTypes.ts
+++ packages/controller/src/modules/schemaTypes.ts
@@ -3,6 +3,29 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL mutation operation: ForgotPasswordChangeMutation
+// ====================================================
+
+export interface ForgotPasswordChangeMutation_forgotPasswordChange {
+  __typename: "Error";
+  path: string;
+  message: string;
+}
+
+export interface ForgotPasswordChangeMutation {
+  forgotPasswordChange: ForgotPasswordChangeMutation_forgotPasswordChange[] | null;
+}
+
+export interface ForgotPasswordChangeMutationVariables {
+  newPassword: string;
+  key: string;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL mutation operation: LoginMutation
 // ====================================================
 
