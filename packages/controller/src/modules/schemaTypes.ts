714f43bac9659973190073ab92e1cfbd397fb7af
leizleho <ho.leizle@gmail.com>
2019-07-27 10:46:03 -0700

    logout controller

--- packages/controller/src/modules/schemaTypes.ts
+++ packages/controller/src/modules/schemaTypes.ts
@@ -32,6 +32,18 @@ export interface LoginMutationVariables {
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL mutation operation: LogoutMutation
+// ====================================================
+
+export interface LogoutMutation {
+  logout: boolean | null;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL mutation operation: RegisterMutation
 // ====================================================
 
