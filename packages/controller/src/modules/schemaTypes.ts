b052b7034d39540388d3537b9649d05baaea389e
leizleho <ho.leizle@gmail.com>
2019-07-27 18:10:19 -0700

    update schematypes

--- packages/controller/src/modules/schemaTypes.ts
+++ packages/controller/src/modules/schemaTypes.ts
@@ -268,6 +268,35 @@ export interface ViewMessagesQueryVariables {
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL subscription operation: NewMessageSubscription
+// ====================================================
+
+export interface NewMessageSubscription_newMessage_user {
+  __typename: "User";
+  id: string;
+  email: string;
+}
+
+export interface NewMessageSubscription_newMessage {
+  __typename: "Message";
+  text: string;
+  user: NewMessageSubscription_newMessage_user;
+  offerId: string;
+}
+
+export interface NewMessageSubscription {
+  newMessage: NewMessageSubscription_newMessage;
+}
+
+export interface NewMessageSubscriptionVariables {
+  offerId: string;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL query operation: ViewOfferQuery
 // ====================================================
 
