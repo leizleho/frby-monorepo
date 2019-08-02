890ed1831b079d69c149a9c50e98ffa97b444500
leizleho <ho.leizle@gmail.com>
2019-08-02 15:49:33 -0700

    add logout module

---
+++ packages/web/src/modules/logout/CallLogout.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface Props {
+  logout: () => void;
+  onFinish: () => void;
+}
+export class CallLogout extends React.PureComponent<Props> {
+  async componentDidMount() {
+    await this.props.logout();
+    this.props.onFinish();
+  }
+  render() {
+    return null;
+  }
+}
