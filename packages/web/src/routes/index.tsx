a36e8758fc6f0a4066a516be242671b6308f1b7b
leizleho <ho.leizle@gmail.com>
2019-08-03 23:47:16 -0700

    offer form

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -3,18 +3,25 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { RegisterConnector } from '../modules/register/RegisterConnector';
 import { LoginConnector } from '../modules/login/LoginConnector';
+import { Logout } from '../modules/logout';
 import SearchOffersConnector from '../modules/offer/search';
 import { CreateOfferConnector } from '../modules/offer/create/CreateOfferConnector';
 
 export const Routes = () => (
   <BrowserRouter>
     <Switch>
+      <Route exact={true} path="/" component={SearchOffersConnector} />
       <Route exact={true} path="/register" component={RegisterConnector} />
       <Route exact={true} path="/login" component={LoginConnector} />
-      <Route exact={true} path="/search" component={SearchOffersConnector} />
+      <Route path="/logout" component={Logout} />
       <Route
         exact={true}
-        path="/create-offer"
+        path="/offers/search"
+        component={SearchOffersConnector}
+      />
+      <Route
+        exact={true}
+        path="/offers/create-offer"
         component={CreateOfferConnector}
       />
     </Switch>
