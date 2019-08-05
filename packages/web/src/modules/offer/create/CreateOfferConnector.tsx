a96b24ef374cb9c36fe467ad2537d1ab8594e7fa
leizleho <ho.leizle@gmail.com>
2019-08-05 01:11:32 -0700

    update create offer

--- packages/web/src/modules/offer/create/CreateOfferConnector.tsx
+++ packages/web/src/modules/offer/create/CreateOfferConnector.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { FormikActions } from 'formik';
+import { withCreateOffer, WithCreateOffer } from '@frby/controller';
+import { OfferFormValues, OfferForm } from '../shared/OfferForm';
 
 import Header from '../../shared/Header/Header';
 import HeaderLinks from '../../shared/Header/HeaderLinks';
 import GridContainer from '../../shared/Grid/GridContainer';
 import GridItem from '../../shared/Grid/GridItem';
-
+import Card from '../../shared/Card/Card';
 import withStyles from '@material-ui/core/styles/withStyles';
 import style from './style';
 import image from '../../../assets/img/bg7.jpg';
@@ -13,7 +17,17 @@ interface Props {
   classes: any;
 }
 
-export class C extends React.PureComponent<Props> {
+export class C extends React.PureComponent<
+  RouteComponentProps<{}> & WithCreateOffer & Props
+> {
+  submit = async (
+    values: OfferFormValues,
+    { setSubmitting }: FormikActions<OfferFormValues>
+  ) => {
+    await this.props.createOffer(values);
+    setSubmitting(false);
+  };
+
   render() {
     const { classes } = this.props;
     return (
@@ -35,7 +49,9 @@ export class C extends React.PureComponent<Props> {
           <div className={classes.container}>
             <GridContainer justify="center">
               <GridItem xs={12} sm={8} md={8}>
-                Create Offer
+                <Card>
+                  <OfferForm submit={this.submit} />
+                </Card>
               </GridItem>
             </GridContainer>
           </div>
@@ -45,4 +61,5 @@ export class C extends React.PureComponent<Props> {
   }
 }
 
-export const CreateOfferConnector = withStyles(style as any)(C);
+const styledCreateOfferForm = withStyles(style as any)(C);
+export const CreateOfferConnector = withCreateOffer(styledCreateOfferForm);
